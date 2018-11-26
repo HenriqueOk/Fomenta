62f2bef35e3f2d0bd6f53df323a2675bd0347a71
Henrique Okomura <henrique.okomura@gmail.com>
2018-11-26 02:56:06 -0200

    Dynamic menu in index

--- html/assets/js/main.js
+++ html/assets/js/main.js
@@ -17,4 +17,24 @@ $(document).ready(function() {
 	})();
 
 	main.init();
+
+	if (localStorage.token) {
+		$("#login").hide();
+		$("#cadastro").hide();
+		if (localStorage.type == "ong") {
+			$(".show-donator").hide();
+		} else {
+			$(".show-ong").hide();
+		}
+	} else {
+		$(".show-ong").hide();
+		$(".show-donator").hide();
+		$(".logout").hide();
+	}
+
+	$(".logout").on("click", function () {
+		localStorage.removeItem('token');
+		localStorage.removeItem('type');
+		location.reload();
+	});
 });
